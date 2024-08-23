90e431e8ed96173dcd76dd91e3ca8ad72bbcc819
dnsamw <dnsam.mit@gmail.com>
2024-08-23 18:52:57 +0530

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+
+      <div className='flex justify-center items-center min-h-screen w-full bg-slate-400'>
+        <h1 className='text-3xl'>The Tree Of Damned</h1>
+      </div>
+
+  )
+}
+
+export default App
